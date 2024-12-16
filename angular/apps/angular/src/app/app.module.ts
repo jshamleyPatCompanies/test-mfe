92d5c9e9e09393150cfcec72deabb8750806d301
Jeff Shamley <jshamley@pattersoncompanies.com>
2024-12-16 11:56:36 -0700

    update to module usage

--- angular/apps/angular/src/app/app.module.ts
+++ angular/apps/angular/src/app/app.module.ts
@@ -4,11 +4,22 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { appRoutes } from './app.routes';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [AppComponent],
-  imports: [NxWelcomeComponent, RouterModule],
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(appRoutes, {
+      initialNavigation: 'enabledBlocking',
+      enableTracing: false,
+      paramsInheritanceStrategy: 'always',
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
